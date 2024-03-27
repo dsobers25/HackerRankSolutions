048685c1af9b8205bc6a2a8ec69b88af26864a58
dsobers25 <deronfsobers2@gmail.com>
2024-03-26 23:07:46 -0500

    added birthday cake candles java file

---
+++ Easy/Java/BirthdayCakeCandles.java
@@ -0,0 +1,27 @@
+package HackerRankSolutions.Easy.Java;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BirthdayCakeCandles {
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+    // Write your code here
+        return Collections.frequency(candles, Collections.max(candles));
+    }
+
+    public static void main(String[] args) {
+        List candles = new ArrayList<>();
+        candles.add(5);
+        candles.add(4);
+        candles.add(5);
+        candles.add(2);
+        candles.add(1);
+        candles.add(5);
+
+
+        System.out.print(birthdayCakeCandles(candles));
+
+    }
+}
